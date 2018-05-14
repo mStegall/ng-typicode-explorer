446d529d1a3ac84b620e466d3f776232bae5f276
Matt Stegall <me@matthewstegall.com>
2018-05-14 17:22:39 -0400

    Add search input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { UserSearchComponent } from './user-search/user-search.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, UserSearchComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/user-search/user-search.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-search',
+  templateUrl: './user-search.component.html',
+  styleUrls: ['./user-search.component.css']
+})
+export class UserSearchComponent implements OnInit {
+
+  constructor() { }
+
+  searchInput:string;
+
+  ngOnInit() {
+  }
+
+}
